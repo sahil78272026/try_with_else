28768d977598950498528ff011ff3dae56dc3aa4
sahil78272026 <sahil.garg78272026@gmail.com>
2022-04-18 21:07:15 +0530

    Add files via upload

---
+++ 04_try_with_else.py
@@ -0,0 +1,12 @@
+try:
+    # print (10/0) # will give error
+    print(10/1)
+
+except Exception as e:
+    print("Invalid Division :" , e)
+
+except Exception as e:
+    print("Invalid Division :" , e)
+
+else:
+    print("Program run succesfully") # will run when try block runs
